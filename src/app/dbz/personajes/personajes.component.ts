e3bf913c103feb47d190a3bc23947269209242b2
alga801560 <alga801560@ALGA801560-11>
2022-08-31 21:23:52 -0500

    Imports are deleted

--- src/app/dbz/personajes/personajes.component.ts
+++ src/app/dbz/personajes/personajes.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { Personaje } from '../interfaces/dbz.interface';
- import { DbzService } from '../services/dbz.service';
+import { Component } from '@angular/core';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-personajes',
